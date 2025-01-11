550c91a917f3298af590bc6c833ecc9556d10f9b
Malith Priyashan <mpriyashan123@gmail.com>
2025-01-11 21:03:48 +0530

    added uers and posts by user

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import { SinglePostPage } from './features/posts/SinglePostPage'
 import { EditPostForm } from './features/posts/EditPostForm'
 
 import { selectCurrentUsername } from './features/auth/authSlice'
+import { UsersList } from './features/users/UsersList'
+import { UserPage } from './features/users/UserPage'
 
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
   const username = useAppSelector(selectCurrentUsername)
@@ -32,6 +34,8 @@ function App() {
                   <Route path="/posts" element={<PostsMainPage />} />
                   <Route path="/posts/:postId" element={<SinglePostPage />} />
                   <Route path="/editPost/:postId" element={<EditPostForm />} />
+                  <Route path="/users" element={<UsersList />} />
+                  <Route path="/users/:userId" element={<UserPage />} />
                 </Routes>
               </ProtectedRoute>
             }
